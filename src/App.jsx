1cbbb9ff5cf463234d4d6bd30cdc69d63cb4b38b
leah <leahtiktin@gmail.com>
2024-12-14 18:45:08 -0700

    touch up the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 import React from 'react'
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Level from "./components/Level";
+import Lightning from "./assets/light.svg";
 //get them to completely fill up the rows
 //change font
 //dont swap after game end
 //adjust card size to round
 function App() {
-  const [level, setLevel] = useState(3);
+  const [level, setLevel] = useState(0);
   const [gameStart, setGameStart] = useState(false);
+  let gameOver = (level == 8);
 
-  const cardsPerLevel = [4, 6, 8, 10, 12, 14, 16,18, 20];
+  const cardsPerLevel = [4, 6, 8, 10, 12, 14, 16,18];
   return (
     <>
-      <h1>Memory Card</h1>
+    <img className = "lightning one" src={Lightning} alt="" />
+    <img className = "lightning two" src={Lightning} alt="" />
+    <img className = "lightning three" src={Lightning} alt="" />
+
+    <div className = {`game-over-message`}></div>
+      <div className="header">
+        <h1>Memory Card</h1>
+      </div>
       <h2>Level {level + 1}</h2>
       <Level level={level} setLevel = {setLevel} 
       key={level} cards={cardsPerLevel[level]}></Level>
